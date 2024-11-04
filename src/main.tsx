fc15ad695686e65a73639c74d09990f0a4b6fc5a
Maciej Glowacki <glowackimaciej97@gmail.com>
2024-11-04 11:38:19 +0100

    removed folder

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { Button } from "./Button.tsx";
 import "./index.css";
 import { RiStarLine } from "@remixicon/react";
-import "remixicon/fonts/remixicon.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
