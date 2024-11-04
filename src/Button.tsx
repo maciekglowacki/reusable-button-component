819a890353e9d66a29f99e704f6badb7aea5ae0f
Maciej Glowacki <glowackimaciej97@gmail.com>
2024-11-04 11:20:36 +0100

    feat: reusable button component

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,3 +1,70 @@
-export function Button() {
-  return <button><button type="submit"></button></button>;
+type IconOnly = {
+  //   Icon: RemixiconComponentType;
+  Icon: React.ReactNode;
+  ariaLabel: string;
+  variant: "icon-only";
+};
+
+type TextOptionalIcon =
+  | {
+      text: string;
+      variant: "text-only";
+    }
+  | {
+      text: string;
+      Icon: React.ReactNode;
+      iconPosition: "left" | "right" | "both";
+      variant: "text-with-icon";
+    };
+
+type Props = {
+  disabled?: boolean;
+  type:
+    | "primary"
+    | "secondary"
+    | "tertiary"
+    | "destructive"
+    | "link-color"
+    | "link-grey";
+  size: "medium" | "large" | "xl" | "xxl";
+} & (TextOptionalIcon | IconOnly);
+
+import "./Button.css";
+
+export function Button(props: Props) {
+  const disabled = props?.disabled || false;
+
+  if (props.variant === "text-only") {
+    return (
+      <button
+        disabled={disabled}
+        className={[props.type, props.size].join(" ")}
+      >
+        {props.text}
+      </button>
+    );
+  }
+  if (props.variant === "text-with-icon") {
+    return (
+      <button
+        disabled={disabled}
+        className={[props.type, props.size].join(" ")}
+      >
+        {(props.iconPosition === "left" || props.iconPosition === "both") &&
+          props.Icon}
+        {props.text}
+        {(props.iconPosition === "right" || props.iconPosition === "both") &&
+          props.Icon}
+      </button>
+    );
+  }
+  return (
+    <button
+      aria-label={props.ariaLabel}
+      disabled={disabled}
+      className={[props.type, props.size, "icon-only"].join(" ")}
+    >
+      {props.Icon}
+    </button>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,236 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Button } from "./Button.tsx";
 import "./index.css";
+import { RiStarLine } from "@remixicon/react";
+import "remixicon/fonts/remixicon.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Button />
+    <div className="main-container">
+      <div className="buttons-wrapper">
+        <div className="row">
+          <Button
+            text="Button CTA"
+            type="primary"
+            size="medium"
+            variant="text-only"
+          />
+          <Button
+            text="Button CTA"
+            type="primary"
+            size="large"
+            variant="text-with-icon"
+            Icon={<RiStarLine size={20} />}
+            iconPosition="left"
+          />
+          <Button
+            text="Button CTA"
+            type="primary"
+            size="xl"
+            variant="text-with-icon"
+            Icon={<RiStarLine size={20} />}
+            iconPosition="right"
+          />
+          <Button
+            text="Button CTA"
+            type="primary"
+            size="xxl"
+            variant="text-only"
+          />
+          <Button
+            type="primary"
+            size="xxl"
+            variant="icon-only"
+            Icon={<RiStarLine size={24} />}
+            ariaLabel="Star button"
+          />
+        </div>
+        <div className="row">
+          <Button
+            text="Button CTA"
+            type="secondary"
+            size="medium"
+            variant="text-only"
+          />
+          <Button
+            text="Button CTA"
+            type="secondary"
+            size="large"
+            variant="text-with-icon"
+            Icon={<RiStarLine size={20} />}
+            iconPosition="left"
+          />
+          <Button
+            text="Button CTA"
+            type="secondary"
+            size="xl"
+            variant="text-with-icon"
+            Icon={<RiStarLine size={20} />}
+            iconPosition="right"
+          />
+          <Button
+            text="Button CTA"
+            type="secondary"
+            size="xxl"
+            variant="text-only"
+          />
+          <Button
+            type="secondary"
+            size="xxl"
+            variant="icon-only"
+            Icon={<RiStarLine size={24} />}
+            ariaLabel="Star button"
+          />
+        </div>
+        <div className="row">
+          <Button
+            text="Button CTA"
+            type="tertiary"
+            size="medium"
+            variant="text-only"
+          />
+          <Button
+            text="Button CTA"
+            type="tertiary"
+            size="large"
+            variant="text-with-icon"
+            Icon={<RiStarLine size={20} />}
+            iconPosition="left"
+          />
+          <Button
+            text="Button CTA"
+            type="tertiary"
+            size="xl"
+            variant="text-with-icon"
+            Icon={<RiStarLine size={20} />}
+            iconPosition="right"
+          />
+          <Button
+            text="Button CTA"
+            type="tertiary"
+            size="xxl"
+            variant="text-only"
+          />
+          <Button
+            type="tertiary"
+            size="xxl"
+            variant="icon-only"
+            Icon={<RiStarLine size={24} />}
+            ariaLabel="Star button"
+          />
+        </div>
+        <div className="row">
+          <Button
+            text="Button CTA"
+            type="link-color"
+            size="medium"
+            variant="text-only"
+          />
+          <Button
+            text="Button CTA"
+            type="link-color"
+            size="large"
+            variant="text-with-icon"
+            Icon={<RiStarLine size={20} />}
+            iconPosition="left"
+          />
+          <Button
+            text="Button CTA"
+            type="link-color"
+            size="xl"
+            variant="text-with-icon"
+            Icon={<RiStarLine size={20} />}
+            iconPosition="right"
+          />
+          <Button
+            text="Button CTA"
+            type="link-color"
+            size="xxl"
+            variant="text-only"
+          />
+          <Button
+            type="link-color"
+            size="xxl"
+            variant="icon-only"
+            Icon={<RiStarLine size={24} />}
+            ariaLabel="Star button"
+          />
+        </div>
+        <div className="row">
+          <Button
+            text="Button CTA"
+            type="link-grey"
+            size="medium"
+            variant="text-only"
+          />
+          <Button
+            text="Button CTA"
+            type="link-grey"
+            size="large"
+            variant="text-with-icon"
+            Icon={<RiStarLine size={20} />}
+            iconPosition="left"
+          />
+          <Button
+            text="Button CTA"
+            type="link-grey"
+            size="xl"
+            variant="text-with-icon"
+            Icon={<RiStarLine size={20} />}
+            iconPosition="right"
+          />
+          <Button
+            text="Button CTA"
+            type="link-grey"
+            size="xxl"
+            variant="text-only"
+          />
+          <Button
+            type="link-grey"
+            size="xxl"
+            variant="icon-only"
+            Icon={<RiStarLine size={24} />}
+            ariaLabel="Star button"
+          />
+        </div>
+        <div className="row">
+          <Button
+            text="Button CTA"
+            type="destructive"
+            size="medium"
+            variant="text-only"
+          />
+          <Button
+            text="Button CTA"
+            type="destructive"
+            size="large"
+            variant="text-with-icon"
+            Icon={<RiStarLine size={20} />}
+            iconPosition="left"
+          />
+          <Button
+            text="Button CTA"
+            type="destructive"
+            size="xl"
+            variant="text-with-icon"
+            Icon={<RiStarLine size={20} />}
+            iconPosition="right"
+          />
+          <Button
+            text="Button CTA"
+            type="destructive"
+            size="xxl"
+            variant="text-only"
+          />
+          <Button
+            type="destructive"
+            size="xxl"
+            variant="icon-only"
+            Icon={<RiStarLine size={24} />}
+            ariaLabel="Star button"
+          />
+        </div>
+      </div>
+    </div>
   </StrictMode>
 );
